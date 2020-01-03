04569fad6a70a27913ccdfa02ee4947a88e167b5
Alex Rock Ancelet <pierstoval@gmail.com>
2020-01-03 16:54:06 +0100

    Remove useless "use" statements in WebTestCase (because of already imported traits)

--- src/WebTestCase.php
+++ src/WebTestCase.php
@@ -3,8 +3,6 @@
 namespace Liior\SymfonyTestHelpers;
 
 use Liior\SymfonyTestHelpers\Concerns\WithAuthenticationTrait;
-use Liior\SymfonyTestHelpers\Concerns\WithClientTrait;
-use Liior\SymfonyTestHelpers\Concerns\WithDatabaseTrait;
 use Liior\SymfonyTestHelpers\Concerns\WithFakerTrait;
 use Liior\SymfonyTestHelpers\Concerns\WithHttpMethodsTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
@@ -12,8 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
 class WebTestCase extends BaseWebTestCase
 {
     use WithAuthenticationTrait;
-    use WithClientTrait;
-    use WithDatabaseTrait;
     use WithFakerTrait;
     use WithHttpMethodsTrait;
 
